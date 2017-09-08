f2b7c438dd267fc0933cae698e0254b7dfda5ac3
epico <hgd.prod@gmail.com>
2017-09-08 08:05:06 +0200

    add values cleanning process

--- GeneaFamilyCore/forms.py
+++ GeneaFamilyCore/forms.py
@@ -6,6 +6,14 @@ from .models import Member
 class MemberForm(ModelForm):
 	""" Create or update member (A.K.A person's identity). """
 
+	def clean_first_name(self):
+		""" remove unnecessary spaces and convert all word in 'Title Case' """
+		return " ".join(self.cleaned_data['first_name'].split()).title()
+
+	def clean_family_name(self):
+		""" remove unnecessary spaces and convert all word in 'Title Case' """
+		return " ".join(self.cleaned_data['family_name'].split()).title()
+
 	class Meta:
 		model = Member
 		fields = [	'first_name',
